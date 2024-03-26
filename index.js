b6a46b91b0dbb7f2e45456f4a9f4a05a6f50326f
mohammadshuraim28 <mohammadshuraim192>
2024-03-26 12:40:56 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter first number", type: "number", name: "firstnumber" },
+    { message: "enter second number", type: "number", name: "secondnumber" },
+    {
+        message: "select one of the following operater to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "substraction", "multiplcation", "division"],
+    },
+]);
+//conditional statement
+if (answer.operator === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "substraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "multiplcation") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("please select valid operator");
+}
